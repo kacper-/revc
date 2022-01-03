48bc884e9c8efe1452a704ebd04b705ffa570058
Kacper Marczewski <kacper.marczewski@gmail.com>
2022-01-03 22:48:42 +0100

    printf

--- ui.h
+++ ui.h
@@ -1,11 +1,13 @@
 #ifndef UI_H
 #define UI_H
 
+#include<cstdio>
 #include "game.h"
 
 uint8_t play(struct game *g)
 {
     // TODO implement human player move using user interface
+    printf("Playing...");
     return 0;
 }
 
